332193cf6faef880ff5b9548d5a7e5e83318b928
Colton Leekley-Winslow <colton@hurricanelabs.com>
2015-01-27 10:09:28 -0500

    dict handler impelemnt emit

--- src/driftwood/handlers/dict.py
+++ src/driftwood/handlers/dict.py
@@ -6,3 +6,6 @@ class DictHandler(logging.Handler):
     def __init__(self, *args, extra_attrs=[], **kwargs):
         super().__init__(*args, **kwargs)
         self._dict_formatter = DictFormatter(extra_attrs=extra_attrs)
+
+    def emit(self, record):
+        return self._dict_formatter.format(record)
