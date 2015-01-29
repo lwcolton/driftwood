45be88ca86525caf0080af454482a6238aff6f10
Colton Leekley-Winslow <colton@hurricanelabs.com>
2015-01-29 16:39:47 -0500

    replace meng with mongoengine

--- src/driftwood/handlers/mongo.py
+++ src/driftwood/handlers/mongo.py
@@ -6,21 +6,21 @@ from driftwood.handlers.dict import DictHandler
 
 class BaseLogRecord(mongoengine.Document):
     meta = {'allow_inheritance': True, 'abstract': True}
-    msecs = meng.FloatField()
-    name = meng.StringField(required=True)
-    levelname = meng.StringField()
-    pathname = meng.StringField()
-    process = meng.IntField()
-    lineno = meng.IntField()
-    relativeCreated = meng.FloatField()
-    funcName = meng.StringField()
-    created = meng.FloatField()
-    message = meng.StringField(required=True)
-    threadName = meng.StringField()
-    filename = meng.StringField()
-    levelno = meng.IntField()
-    thread = meng.LongField()
-    module = meng.StringField()
+    msecs = mongoengine.FloatField()
+    name = mongoengine.StringField(required=True)
+    levelname = mongoengine.StringField()
+    pathname = mongoengine.StringField()
+    process = mongoengine.IntField()
+    lineno = mongoengine.IntField()
+    relativeCreated = mongoengine.FloatField()
+    funcName = mongoengine.StringField()
+    created = mongoengine.FloatField()
+    message = mongoengine.StringField(required=True)
+    threadName = mongoengine.StringField()
+    filename = mongoengine.StringField()
+    levelno = mongoengine.IntField()
+    thread = mongoengine.LongField()
+    module = mongoengine.StringField()
 
 class LogRecord(BaseLogRecord):
     pass
