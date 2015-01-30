b576341285c19a82ca179c1a9658650d48213f90
Colton Leekley-Winslow <colton@hurricanelabs.com>
2015-01-29 20:46:58 -0500

    add readme to setup

--- setup.py
+++ setup.py
@@ -6,10 +6,14 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, "version"), "r") as version_handle:
     version = version_handle.read().strip()
 
+readme_path = os.path.join(here, 'README.rst')
+readme = open(readme_path).read()
+
 setup(
 	name = "driftwood",
 	version = version,
 	description = "A collection of python logging extensions",
+    long_description = readme,
 	url = "https://github.com/HurricaneLabs/driftwood",
 	author = "Colton Leekley-Winslow",
 	author_email = "colton@hurricanelabs.com",
