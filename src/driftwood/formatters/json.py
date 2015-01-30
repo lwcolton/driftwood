cab528f69ac0c09d532f8e3dd5e9916247a743ff
Colton Leekley-Winslow <colton@hurricanelabs.com>
2015-01-29 19:54:06 -0500

    fix json formatter docstring

--- src/driftwood/formatters/json.py
+++ src/driftwood/formatters/json.py
@@ -5,8 +5,7 @@ from driftwood.formatters.dict import DictFormatter
 class JSONFormatter(DictFormatter):
     """Formats messages as JSON.
 
-    This class is a subclass of :py:class:`driftwood.formatters.DictFormatter`,
-    and accepts the same arguments for __init__.
+    Accepts the same arguments as :class:`~driftwood.formatters.dict.DictFormatter`
     """
     def format(self, record):
         message_dict = super().format(record)
